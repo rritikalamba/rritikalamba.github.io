a74e58aee25ee803cdb5725edadf1f84805a75d9
rritikalamba <ritikalambawork@gmail.com>
2024-09-26 12:43:10 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
+// next.config.js
 module.exports = {
-    // Use the 'export' mode for static file generation
-    output: 'export',
-    images: {
-      unoptimized: true, // This disables the Image Optimization API, as GitHub Pages does not support it
-    },
-    basePath: '/rritikalamba.github.io', // Set your repository name
-    assetPrefix: '/rritikalamba.github.io', // Set your repository name
-  };
-  
+  output: 'export',  // Export static files for GitHub Pages
+  images: {
+    unoptimized: true,  // Disable Next.js image optimization
+  },
+  basePath: '/rritikalamba.github.io',  // Replace with your repository name
+  assetPrefix: '/rritikalamba.github.io',  // Replace with your repository name
+};
